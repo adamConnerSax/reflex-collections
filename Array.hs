402b0f187f0bb7633c34e9518f3c07ed97896403
adamConnerSax <adam_conner_sax@yahoo.com>
2018-07-25 12:44:39 -0400

    Working on collection functions...

--- Array.hs
+++ Array.hs
@@ -10,7 +10,7 @@
 {-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE UndecidableInstances       #-}
-module Reflex.Dom.Contrib.ListHoldFunctions.Maps
+module Reflex.Dom.Contrib.ListHoldFunctions.Array
   (
   , listHoldWithKeyArray
   , listWithKeyShallowDiffArray
@@ -46,44 +46,48 @@ import           Data.Align                                (Align (..))
 import           Data.Maybe                                (isNothing)
 import           Data.These                                (These (..))
 
+arrayMapWithKey :: Ix k => (k -> v -> a) -> A.Array k v -> A.Array k a
+arrayMapWithKey h a =
+  let kvPairs = assocs a
+      mapped = (\(k,v) -> (k, h k v)) <$> kvPairs
+  in A.array (min . fst $ kvPairs, max . fst $ kvPairs) mapped
 
 arrayFan:: (A.Ix k, R.Reflex t) => R.Event t (Array k v) -> R.EventSelector t (Const2 k v)
 arrayFan = R.fan . fmap arrayToDMap
 
 arrayWithFunctorToDMap :: A.Ix k => A.Array k v -> DMap (Const2 k v) f
-arrayWithFunctortoDMap = DM.fromList . fmap (\(k, v) -> Const2 k := v) . F.toList
+arrayWithFunctorToDMap = DM.fromList . fmap (\(k, v) -> Const2 k := v) . F.toList
 
 arrayToDMap :: Ix k => A.Array k v -> DMap (Const2 k v) Identity
 arrayToDMap = arrayWithFunctorToDMap . fmap Identity
 
-dmapToArray :: Ix k => DMap (Const2 k v) Identity -> Array k v
+dmapToArray :: Ix k => DMap (Const2 k v) Identity -> A.Array k v
 dmapToArray dm =
   let kvPairs = fmap (\(Const2 k :=> Identity v) -> (k, v)) $ DM.toList dm
       keys = fst <$> kvPairs
       in A.array (min keys, max keys) kvPairs
 
-
 distributeArrayOverDynPure::(R.Reflex t, Ix k) => A.Array k (R.Dynamic t v) -> R.Dynamic t (A.Array k v)
 distributeArrayOverDynPure = fmap dmapToArray . R.distributeDMapOverDynPure . arrayWithFunctorToDMap
+
 -- HERE
-instance Align f => Align (WrapMap f) where
-  nil = WrapMap nil
-  align a b = WrapMap $ align (unWrapMap a) (unWrapMap b)
 
-newtype WrapA a = WrapA { unWrapA::a }  -- also to avoid overlapping instances
+newtype ArrayDiff k v = ArrayDiff { diff :: [(k, Maybe v)] }
 
-fromWrapA::Functor g=>(k -> v -> g (WrapA a))->(k->v->g a)
-fromWrapA f x y = unWrapA <$> f x y
+instance Functor (ArrayDiff k) where
+  fmap f = ArrayDiff . fmap (\(k, mv) -> (k, fmap f mv)) . diff
 
+arrayDiffMapWithKey :: (k -> v -> a) -> ArrayDiff k v -> ArrayDiff k a
+arrayDiffMapWithKey h = ArrayDiff . fmap (\(k, mv) -> (k, fmap (h k) mv)) . diff
 
-instance (LHFMap (WrapMap f), LHFMapKey (WrapMap f) ~ k)=>ToPatchType (WrapMap f) k v (WrapA a) where
-  type Diff (WrapMap f) k = Compose (WrapMap f) Maybe
-  type SeqType (WrapMap f) k = DM.DMap
-  type SeqPatchType (WrapMap f) k = PatchDMap
-  type SeqTypeKey (WrapMap f) k (WrapA a) = Const2 k a
-  toSeqTypeWithFunctor h = lhfMapWithFunctorToDMap . lhfMapWithKey (fromWrapA h)
-  makePatchSeq _ h = PatchDMap . lhfMapWithFunctorToDMap . lhfMapWithKey (\k mv -> ComposeMaybe $ fmap (fromWrapA h k) mv) . getCompose
-  fromSeqType _ _ = fmap WrapA . lhfDMapToMap
+instance Ix k => ToPatchType (A.Array k) k v a where
+  type Diff (A.Array k) k = ArrayDiff k
+  type SeqType (Array k) k = DM.DMap
+  type SeqPatchType (Array k) k = PatchDMap
+  type SeqTypeKey (A.Array k) k a = Const2 k a
+  toSeqTypeWithFunctor h = arrayWithFunctorToDMap . arrayMapWithKey h
+  makePatchSeq _ h = PatchDMap . DM.fromList . diff . arrayDiffMapWithKey h
+  fromSeqType _ _ = dmapToArray
 
 instance (LHFMap (WrapMap f), Align (WrapMap f), Functor (WrapMap f))=>Diffable (WrapMap f) (Compose (WrapMap f) Maybe) where
   emptyContainer _ = lhfEmptyMap
