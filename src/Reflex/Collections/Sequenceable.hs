8aee15df956f072ec8b4d2b023f3be2925c6c4fc
adamConnerSax <adam_conner_sax@yahoo.com>
2018-08-08 14:15:44 -0400

    Blah.  IntMap and DMap are very different looking types...

--- src/Reflex/Collections/Sequenceable.hs
+++ src/Reflex/Collections/Sequenceable.hs
@@ -16,7 +16,7 @@ module Reflex.Collections.Sequenceable
   ) where
 
 import qualified Reflex                 as R
-import           Reflex.Patch           (PatchDMap (..))
+import           Reflex.Patch           (PatchDMap (..), PatchIntMap)
 
 import           Reflex.Collections.DMapIso (DMapIso (..))
 
@@ -24,6 +24,8 @@ import           Data.Dependent.Map     (DMap, GCompare)
 import           Data.Functor.Misc      (Const2 (..))
 import           Control.Monad.Identity (Identity (..))
 import           Data.Kind              (Type)
+import           Data.IntMap            (IntMap)
+import Data.Functor.Compose (Compose(..), getCompose)
 
 
 -- | This class carries the ability to do an efficient event merge and also sequence a collection of Dynamics.
@@ -43,9 +45,11 @@ instance GCompare k => ReflexSequenceable (DMap k) where
 -- patchPairToDynamic is a sort of inverse, turning a static d containing values and events with patches to it, new values at some keys,
 -- and returns an adjustable monad containing a Dynamic of a value containing d.
 -- |
+
+
 class ( R.Patch (pd Identity)
       , R.PatchTarget (pd Identity) ~ d Identity)
-  => PatchSequenceable (d :: (Type -> Type) -> Type) (pd :: (Type -> Type) -> Type)  where
+  => PatchSequenceable d pd  where
   sequenceWithPatch :: R.Adjustable t m => d m -> R.Event t (pd m) -> m (d Identity, R.Event t (pd Identity))
   patchPairToDynamic :: (R.MonadHold t m, R.Reflex t) => d Identity -> R.Event t (pd Identity) -> m (R.Dynamic t (d Identity))
 
@@ -62,4 +66,27 @@ instance (GCompare (Const2 k a), Ord k) => PatchSequenceable (DMap (Const2 k a))
                      -> R.Event t (PatchDMap (Const2 k a) Identity)
                      -> m (R.Dynamic t (DMap (Const2 k a) Identity))
   patchPairToDynamic a0 a' = R.incrementalToDynamic <$> R.holdIncremental a0 a'
+
+
+newtype ComposedIntMap a f = ComposedIntMap { unCI :: Compose IntMap f a }
+newtype ComposedPatchIntMap a f = ComposedPatchIntMap { unCPI :: Compose PatchIntMap f a }
+
+instance Patch (ComposedPatchIntMap a Identity) where
+  type PatchTarget (ComposedPatchIntMap a Identity) = ComposedIntMap a Identity
+  
+
+  
+instance PatchSequenceable (ComposedIntMap a) (ComposedPatchIntMap a) where
+  {-
+  sequenceWithPatch :: R.Adjustable t m
+                    => ComposedIntMap m a
+                    -> R.Event t (ComposedPatchIntMap m a)
+                    -> m (ComposedIntMap Identity a, R.Event t (ComposedPatchIntMap Identity a))
+--  sequenceWithPatch ci cpEv = R.traverseIntMapWithKeyWithAdjust $ \_ -> fmap Identity 
+-}
+  patchPairToDynamic :: (R.MonadHold t m, R.Reflex t)
+                     => ComposedIntMap Identity a
+                     -> R.Event t (ComposedPatchIntMap Identity a)
+                     -> m (R.Dynamic t (ComposedIntMap Identity a))
+  patchPairToDynamic a0 a' = fmap (fmap (ComposedIntMap . Compose . fmap Identity)) $ R.incrementalToDynamic <$> R.holdIncremental (fmap runIdentity . getCompose . unCI $ a0) (fmap runIdenity . getCompose . unCPI $ a')
 
